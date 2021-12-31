14fcb10f258bc9447c4aa84045b06afb0f5d4c45
Chammi Hansana <chammi2819@gmail.com>
2021-12-31 22:16:15 +0530

    if clause

--- Make clock using c++.cpp
+++ Make clock using c++.cpp
@@ -17,6 +17,28 @@ int main()
         err++;
         else
         system("cls");
-
     }
+    while(a==0)
+    {
+        system("cls");
+        cout<<h<<":"<<m<<":"<<s<<endl;
+        Sleep(1000);
+        s++;
+        if(s>59)
+        {
+            s=0;
+            m++;
+        }
+        if(m>59)
+        {
+            m=0;
+            h++;
+        }
+        if(h>24)
+        {
+            h=0;
+           
+        }
+    }
+    return 0;
 }
